30a665215a7f35e70396780b9dafa6da82bf5b07
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-04-22 11:46:29 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from "./screens/Home";
-import StartMapScreen from "./screens/StarMap";
-import DailyPicScreen from "./screens/DailyPic";
-import SpaceCraftScreen from "./screens/SpaceCraft";
+import HomeScreen from "./pantalla/Inicio";
+import StartMapScreen from "./pantallas/MapaEstelar";
+import DailyPicScreen from "./pantallas/FotoDiaria";
+import SpaceCraftScreen from "./pantallas/Nave espacial";
 
 const Stack = createStackNavigator();
 
